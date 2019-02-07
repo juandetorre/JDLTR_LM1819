e49bc6920cbeea4dc4d4154d8e4f1935f1882d50
Alumno <juandetorre11@gmail.com>
2019-02-07 08:48:13 +0100

    lo

---
+++ Deberes/datos/Php/ejer7.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $a = 1;
+    $b = 2;
+    $c = 3;
+    $d = 4;
+    $e = 5;
+    $f = 6;
+    $g = 7;
+    $h = 8;
+    $i = 9;
+    $j = 10;
+
+    $fir = 10;
+    while ($fir >= 1) {
+        $mul = $a * $fir;
+        echo  "$a por $fir es igual a $mul";
+        $fir++;
+    }
+
+?>
+</body>
+</html>
